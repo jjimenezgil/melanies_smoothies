4f8f707034d20ae8750b36197fe568f65865121d
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-03-05 12:11:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,4 @@ if ingredients_list:
 
 # New section to display fruityvice nutrition info
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())

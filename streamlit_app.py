5f2804b3e93cfee01be2784935aad121ec332860
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-03-05 12:38:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Obtain session for Streamlit not in Snowflake
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', 
